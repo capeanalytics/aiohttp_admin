bf29b796e665911436b02b01c8a9d79cdd713d95
Manuel Hernandez <manuel@capeanalytics.com>
2019-10-11 11:25:59 +0200

    Hotfix for error in dd_trace

--- aiohttp_admin/consts.py
+++ aiohttp_admin/consts.py
@@ -4,4 +4,5 @@ import pathlib
 APP_KEY = 'aiohttp_admin'
 TEMPLATES_ROOT = pathlib.Path(__file__).parent / 'templates'
 PROJ_ROOT = pathlib.Path(__file__).parent
-TEMPLATE_APP_KEY = 'aiohttp_admin_templates'
+#  TEMPLATE_APP_KEY = 'aiohttp_admin_templates'
+TEMPLATE_APP_KEY = 'aiohttp_jinja2_environment'
